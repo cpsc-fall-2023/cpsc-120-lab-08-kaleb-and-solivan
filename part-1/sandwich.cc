7039256b79181ed95a4fcb8955a1708428915cc3
Kaleb Robles <kkrobles@csu.fullerton.edu>
2023-10-30 15:06:35 -0700

    lab 8 part 1

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
 
   std::string protein = arguments[1];
   std::string bread = arguments[2];
-  std::string condiment = arguments[2];
+  std::string condiment = arguments[3];
 
   std::cout << "Your order: A " << protein << " sandwich on " << bread
             << " with " << condiment << "."
